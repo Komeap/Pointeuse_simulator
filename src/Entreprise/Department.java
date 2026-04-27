55b7f9f89d6033fac8dc4efd172ad8065c6bd67b
Pnd <pinaudevan@gmail.com>
2026-04-27 12:15:38 +0200

    add Departement class

--- src/Entreprise/Department.java
+++ src/Entreprise/Department.java
@@ -1,14 +1,43 @@
 package Entreprise;
 import Employee.Employee;
-
 import java.util.List;
+import java.util.ArrayList;
 
 public class Department {
 
-	private String depName;
+	private String depName; //name of departement
 
-	private List employeeList;
+	private List <Employee> employeeList; //list of employees who are in this departement
 
-	private Employee[] employee;
+	//constructor of Departement
+	public Department(String sDepName)
+	{
+		this.depName = sDepName;
 
+		this.employeeList = new ArrayList<>();
+	}
+
+	//- - - ATTRIBUTE - - -
+		//attribute depName
+	public String getDepartement(){ return this.depName;} //allows access to the depName value
+
+	public void setDepartement(String sDepName){ this.depName = sDepName;} //allows edit to the depName value
+
+		//attribute employeeList
+	public List <Employee> getEmployeeList(){ return new ArrayList<>(employeeList);} //allows access to the employeeList list
+
+	//allows add a new employee in the employeeList list
+	public void addEmployee(Employee employee)
+	{
+		if (employee != null) {
+			this.employeeList.add(employee);
+		}
+	}
+
+	//allows remove a employee in the employeeList list
+	public void removeEmployee(Employee employee)
+	{
+		this.employeeList.remove(employee);
+	}
 }
+
